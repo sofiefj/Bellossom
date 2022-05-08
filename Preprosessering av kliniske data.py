8fd07d8289a74874ae286f5983204b82521c7fa1
sofiefj <78593119+sofiefj@users.noreply.github.com>
2022-05-08 14:48:45 +0200

    Update and rename Preprosessering av kliniske data to Preprosessering av kliniske data.py

--- Preprosessering av kliniske data.py
+++ Preprosessering av kliniske data.py
@@ -4,8 +4,13 @@ import pandas as pd
 # Laste inn klinisk data fra OUS i en pandas dataframe
 klin_df = pd.read_excel('data/tabular_data/ous/clinical_ous.xlsx')
 klin_df= pd.DataFrame(klin_df)
+
+# Sette indeks lik pasient-ID
 klin_df = klin_df.set_index(('patient_id'))
+
+# Fjerne egenskapen ECOG fra datasettet
 klin_df = klin_df.drop(['ecog'], axis=1)
 
+# Fylle manglende verdier av hpv_related og uicc8_III-IV med verdien 2.
 klin_df['hpv_related'] = klin_df['hpv_related'].fillna(2)
 klin_df['uicc8_III-IV'] = klin_df['uicc8_III-IV'].fillna(2)
